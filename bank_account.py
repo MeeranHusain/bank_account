72629926bcc967ce199cc9a8421f8f76ecb7ec49
Meeran Husain <90550959+MeeranHusain@users.noreply.github.com>
2024-10-08 13:56:53 +0530

    Add files via upload

---
+++ bank_account.py
@@ -0,0 +1,42 @@
+class BalanceException(Exception):
+    pass 
+
+class BankAccount:
+    def __init__(self, initialAmount, acctName):
+        self.balance = initialAmount
+        self.name = acctName
+        print(f"\nAccount '{self.name}' created.\nBalance = ₹{self.balance:.2f}")
+        
+    def getBalance(self):
+        print(f"\nAccount '{self.name}' balance = ₹{self.balance:.2f}")
+        
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("\nDeposit complete.")
+        self.getBalance()
+        
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceException(f"\nSorry, account '{self.name}' only has a balance of ₹{self.balance:.2f}")
+        
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount
+            print("\nWithdraw complete.")
+            self.getBalance()
+        except BalanceException as error:
+            print(f"\nWithdraw interrupted: {error}")
+            
+    def transfer(self, amount, account):
+        try:
+            print('\n**********\n\nBeginning Transfer.. 🚀')
+            self.viableTransaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
+            print("\nTransfer complete! ✅\n\n**********")
+            
+        except BalanceException as error:
+            print(f"\nTransfer interrupted. ❌ {error}")
